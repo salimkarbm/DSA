29ce44c6ff7355e443a6485a991aacaddaaf97f3
salim imuzai <salimimuzai@gmail.com>
2022-05-05 11:21:22 +0100

    add solution 9 One Awesome message

--- exercises/dataTypesAndVariables.js
+++ exercises/dataTypesAndVariables.js
@@ -230,3 +230,11 @@ var game = `The Intro to JavaScript course is ${adjective1}. James and Julia are
  */
 
 // Add your code here
+
+//SOLUTION
+var firstName = 'Julia'
+var interest = 'cats'
+var hobby = 'play video games'
+
+var awesomeMessage = `Hi, my name is ${firstName}. I love ${interest}. In my spare time, I like to ${hobby}.`
+//console.log(awesomeMessage)
