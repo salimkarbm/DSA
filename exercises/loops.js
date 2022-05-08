55686ec47084d46074c92dbc309bd005ba1f689f
salim imuzai <salimimuzai@gmail.com>
2022-05-08 17:29:59 +0100

    add solution 3 Countdown, Liftoff

--- exercises/loops.js
+++ exercises/loops.js
@@ -143,6 +143,26 @@ function bottles(num) {
  * - BE CAREFUL ABOUT THE PUNCTUATION AND THE EXACT WORDS TO BE PRINTED.
  */
 // your code goes here
+//SOLUTION
+let time = 60
+while (time >= 0) {
+  if (time === 50) {
+    console.log('Orbiter transfers from ground to internal power')
+  } else if (time === 31) {
+    console.log('Ground launch sequencer is go for auto sequence start')
+  } else if (time === 16) {
+    console.log('Activate launch pad sound suppression system')
+  } else if (time === 10) {
+    console.log('Activate main engine hydrogen burnoff system')
+  } else if (time === 6) {
+    console.log('Main engine star')
+  } else if (time === 0) {
+    console.log('Solid rocket booster ignition and liftoff!')
+  } else {
+    console.log(`T-${time} Seconds`)
+  }
+  time--
+}
 
 // 4. Rewrite the following while loop as a for loop:
 // var x = 9;
